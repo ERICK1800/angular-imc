4b274f82359ae2ffdcdbcf2616d67ca012593938
ERICK1800 <ericklimaflorencio@gmail.com>
2023-03-30 17:39:23 -0300

    calculo

--- src/app/imc/imc.component.ts
+++ src/app/imc/imc.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class ImcComponent {
 
+  idade : number = 0;
+  altura : number = 0;
+  peso : number = 0;
+  resup : number = 0;
+
+  calcular(){
+
+    this.resup = this.peso / (this.altura * this.altura);
+
+  }
+
 }
